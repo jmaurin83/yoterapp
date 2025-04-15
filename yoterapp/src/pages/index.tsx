8737bd7db91552855d41213a5994e5a5cad55153
jmaurin83 <jmaurin83@orange.fr>
2025-04-15 17:43:36 +0400

    Update index.tsx
    Fix composant page d’accueil avec import dynamique

--- yoterapp/src/pages/index.tsx
+++ yoterapp/src/pages/index.tsx
@@ -1,4 +1,9 @@
-import DashboardView from "../components/DashboardView";
+import dynamic from "next/dynamic";
+
+// On importe dynamiquement le composant pour éviter les erreurs au build
+const DashboardView = dynamic(() => import("../components/DashboardView"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <DashboardView />;
